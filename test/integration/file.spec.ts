bdd4c105be8d3f353acbaf6e4c6e2ef63d336eaa
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2024-10-02 22:46:31 +0200

    test: bee timeout raised to 60s

--- test/integration/file.spec.ts
+++ test/integration/file.spec.ts
@@ -6,7 +6,7 @@ import { DEFAULT_MAX_PAYLOAD_SIZE } from '../../src/chunk'
 import { bytesToHex } from '../../src/utils'
 
 const beeUrl = process.env.BEE_API_URL || 'http://localhost:1633'
-const bee = new Bee(beeUrl)
+const bee = new Bee(beeUrl, { timeout: 60000 })
 const stamp = process.env.BEE_POSTAGE
 if (!stamp) {
   throw new Error('BEE_POSTAGE system environment variable is not defined')
